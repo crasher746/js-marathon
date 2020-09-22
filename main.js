7ee77acff781cc953e974c78f322abc2dc190926
crasher746 <crasher746@gmail.com>
2020-09-22 14:32:38 +0700

    My First Commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// console.log("%cString", "color: #FFFF00; background-color: #5394EC; padding: 4px; font-size: 16px;");
+//
+// // String(5);
+// console.log(typeof String(5));
+//
+// const hello = 'Hello';
+// const world = 'World';
+// const concat = hello + world;
+// console.log(concat + 5);
+// console.log(concat + true);
+// console.log(concat + null);
+// console.log(concat + undefined);
+// console.log(concat + function () {});
+// console.log(concat + [1, 2, 3, 4]);
+// console.log(concat + {});
+
+console.log("%cNumber()", "color: #FFFF00; background-color: #5394EC; padding: 4px; font-size: 16px;");
+// Number('5');
+console.log(Number('5'));
+console.log(+'6');
+console.log(typeof +'6');
+console.log(Number('asd'));
+console.log(+true);
+console.log(+false);
+console.log(+null);
+console.log(Boolean(1));
+
+let currentYear = 2020;
+
+console.log(currentYear++);
+console.log(currentYear);
+console.log(++currentYear);
