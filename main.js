915eb1d233dd8bba005f1af3d93ba25ac63f61da
crasher746 <crasher746@gmail.com>
2020-09-30 02:00:06 +0700

    Add functions to objects using this

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const character = {
     damageHP: 100,
     elHP: document.getElementById('health-character'),
     elProgressbar: document.getElementById('progressbar-character'),
+    renderHP: renderHP,
+    renderHPLife: renderHPLife,
+    renderProgressbarHP: renderProgressbarHP,
+    changeHP: changeHP,
+    ultimate: ultimate,
 };
 
 const enemy = {
@@ -14,66 +19,71 @@ const enemy = {
     damageHP: 100,
     elHP: document.getElementById('health-enemy'),
     elProgressbar: document.getElementById('progressbar-enemy'),
+    renderHP: renderHP,
+    renderHPLife: renderHPLife,
+    renderProgressbarHP: renderProgressbarHP,
+    changeHP: changeHP,
+    ultimate: ultimate,
 };
 
 $btn.addEventListener('click', function () {
     console.log('kick D:');
     random();
-    changeHP(random(20), character);
-    changeHP(random(20), enemy);
+    character.changeHP(random(20));
+    enemy.changeHP(random(20));
 });
 
 $btnUlt.addEventListener('click', function () {
-   ultimate(character);
-   ultimate(enemy);
+   character.ultimate();
+   enemy.ultimate()
 });
 
 function init() {
     console.log('Start game');
     $btnUlt.disabled = true;
-    renderHP(character);
-    renderHP(enemy);
+    character.renderHP();
+    enemy.renderHP();
 };
 
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressbarHP(person);
-    if (person.damageHP < 50 && person.damageHP > 2) $btnUlt.disabled = false;
-    if (person.damageHP === 1) {$btnUlt.disabled = true;}
+function renderHP() {
+    this.renderHPLife();
+    this.renderProgressbarHP();
+    if (this.damageHP < 50 && this.damageHP > 2) $btnUlt.disabled = false;
+    if (this.damageHP === 1) {$btnUlt.disabled = true;}
 };
 
-function renderHPLife (person) {
-    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+function renderHPLife () {
+    this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
 };
 
-function renderProgressbarHP (person) {
-    person.elProgressbar.style.width = person.damageHP + '%';
+function renderProgressbarHP () {
+    this.elProgressbar.style.width = this.damageHP + '%';
 };
 
-function changeHP (count, person) {
-    if (person.damageHP < count) {
-        person.damageHP = 0;
-        alert('Бедный ' + person.name + 'проиграл D:');
+function changeHP (count) {
+    if (this.damageHP < count) {
+        this.damageHP = 0;
+        alert('Бедный ' + this.name + ' проиграл D:');
         $btn.disabled = true;
         $btnUlt.disabled = true;
     } else {
-        person.damageHP -= count;
+        this.damageHP -= count;
     }
-    renderHP(person);
+    this.renderHP();
 };
 
 function random(num) {
     return Math.ceil(Math.random() * num);
 };
 
-function ultimate(person) {
-    if (person.damageHP < 50) {
+function ultimate() {
+    if (this.damageHP < 50) {
         $btnUlt.disabled = false;
-        person.damageHP -= (person.damageHP - 1);
+        this.damageHP -= (this.damageHP - 1);
     } else {
         $btnUlt.disabled = true;
     }
-    renderHP(person);
+    this.renderHP();
 };
 
 init();
