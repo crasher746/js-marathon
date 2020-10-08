55828f17917e2f059b749c8bb800f1e2668112d9
crasher746 <crasher746@gmail.com>
2020-10-09 02:41:44 +0700

    Some changes

--- src/main.js
+++ src/main.js
@@ -1,109 +1,51 @@
-import random from "./utils";
 import Pokemon from "./pokemon.js";
+import {pokemons} from "./pokemons";
+import {random, countClick, generateLog} from "./utils";
 
-const $btn = document.getElementById('btn-kick');
-const $btnUlt = document.getElementById('btn-ultimate');
-
-/*const $logs = document.querySelector('#logs');*/
-
-const [clickOnKick, textOnButtonKick] = countClick();
-const [clickOnUltimate, textOnButtonUltimate] = countClick();
+const pikachu = pokemons.find(item => item.name === 'Pikachu');
+console.log(pikachu);
 
 const player1 = new Pokemon({
-    name: 'Pikachu',
-    defaultHP: 300,
-    damageHP: 300,
+    ...pikachu,
+    hp: 300,
 
-    selectors: 'character',
+    selectors: 'player1',
+});
+
+const $control = document.querySelector('.control');
+console.log(player1);
+player1.attacks.forEach(item => {
+    console.log(item);
+    const $btn = document.createElement('button');
+    $btn.classList.add('button');
+    $btn.innerText = item.name;
+    const btnCount = countClick(item.maxCount, $btn);
+    $btn.addEventListener('click', () => {
+        console.log('Click button', $btn.innerText);
+        btnCount();
+        player1.changeHP(random(item.maxDamage, item.minDamage), function (count, current, total) {
+            console.log('Some change after change HP', count );
+            console.log(generateLog(player1, player2, count, current, total));
+        });
+        player2.changeHP(random(item.maxDamage, item.minDamage), function (count, current, total) {
+            console.log('Some change after change HP', count );
+            console.log(generateLog(player2, player1, count, current, total));
+        });
+    })
+
+    $control.appendChild($btn);
 });
 
 const player2 = new Pokemon({
     name: 'Charmander',
-    defaultHP: 500,
-    damageHP: 500,
+    hp: 500,
 
-    selectors: 'enemy',
+    selectors: 'player2',
 });
 console.log(player1);
 console.log(player2);
 
-function countClick() {
-    let counter = 0;
-    return [
-        function (number, typeOfHit) {
-            counter += number
-            console.log(`${typeOfHit}` + '[' + counter + ']');
-            return counter;
-        },
-        function (id, limit, button) {
-            if (counter === limit) {button.disabled = true;}
-            return document.getElementById(`${id}`).innerText = '(' + counter + '/' + limit + ' )';
-        }
-    ]
-};
 
-$btn.addEventListener('click', function () {
-    player1.changeHP(random(60, 20), function (count, damageHP, defaultHP) {
-        console.log('Some change after change HP', count );
-        console.log(generateLog(player1, player2, count, damageHP, defaultHP));
-    });
-    player2.changeHP(random(60, 20), function (count, damageHP, defaultHP) {
-        console.log('Some change after change HP', count );
-        console.log(generateLog(player2, player1, count, damageHP, defaultHP));
-    });
 
-    clickOnKick(1, 'Kick');
-    textOnButtonKick('clickOnKick', 6, $btn);
-});
 
-$btnUlt.addEventListener('click', function () {
-    player1.changeHP(100, function (count, damageHP, defaultHP) {
-        console.log('Some change after change HP', count );
-        console.log(generateLog(player1, player2, count, damageHP, defaultHP));
-    });
-    player2.changeHP(100, function (count, damageHP, defaultHP) {
-        console.log('Some change after change HP', count );
-        console.log(generateLog(player2, player1, count, damageHP, defaultHP));
-    });
 
-    clickOnUltimate(1, 'Ultimate');
-    textOnButtonUltimate('clickOnUltimate', 1, $btnUlt);
-});
-
-function generateLog(firstPerson, secondPerson, damage, damageHP, defaultHP) {
-    const logs = [
-        `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${damage}, [${damageHP}/${defaultHP}]`,
-        `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage}, [${damageHP}/${defaultHP}]`
-    ];
-
-    return logs[random(logs.length) - 1];
-};
-
-/*function changeHP (count) {
-    let { name, defaultHP, damageHP } = this;
-
-    this.damageHP -= count;
-
-    const log = this === enemy ? generateLog(this, character, count, damageHP, defaultHP) : generateLog(this, enemy, count, damageHP, defaultHP);
-
-    const $p = document.createElement('p');
-    $p.innerText = log;
-    $logs.insertBefore($p, $logs.children[0]);
-
-    if (this.damageHP <= 0) {
-        this.damageHP = 0;
-        alert('Бедный ' + name + ' проиграл D:');
-        $btn.disabled = true;
-        $btnUlt.disabled = true;
-    }
-
-    this.renderHP();
-};*/
